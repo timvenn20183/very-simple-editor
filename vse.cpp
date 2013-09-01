dbbda0dfa2529104b2f809ec985bed2b3a682a6b
Timothy Venn <tim@nevyn.co.za>
2013-09-01 22:08:49 +0200

    Added vse_file and also updated the concept blog

--- vse.cpp
+++ vse.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <stdio.h>
+#include "vse_file.cpp"
 
 using namespace std;
+
+// constants
+const int characters_per_block = 80;                
+
+// structures
+typedef struct Block
+{
+    char content[characters_per_block];
+    struct Block *next;
+    struct Block *prev;
+}block;
 
 int main(int argc, char* argv[]) {
     return 0;
---
+++ vse_file.cpp
@@ -0,0 +1,14 @@
+/*
+
+This handles all the file i/o operations
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int read_file()
+{
+
+}
