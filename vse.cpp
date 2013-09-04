c25e1251cbdba042b0fd9e52c9e9bce5698f27f7
Timothy Venn <tim@nevyn.co.za>
2013-09-04 21:38:59 +0200

    Moved all code to single file for initial coding. Bad practice but makes it easier for me.

--- vse.cpp
+++ vse.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <stdio.h>
-#include "vse_file.cpp"
-#include "vse_scrn.cpp"
 
 using namespace std;
 
@@ -16,6 +14,62 @@ typedef struct Block
     struct Block *prev;
 }block;
 
+// globals
+static block *start_pointer = (block *)malloc(sizeof(block));
+
+// FILE ROUTINES
+
+// read file into memory
+int read_file()
+{
+    // stub
+}
+
+// write file to disk
+int write_file()
+{
+    // stub
+}
+
+// MEMORY ROUTINES
+
+// add a new block 
+void append_new_block()
+{
+    // stub
+}
+
+// return the last block
+block last_block()
+{
+    // stub
+    block *pointer = start_pointer;
+    return block
+}
+
+// SCREEN ROUTINES
+
+// setup current screen 
+void init_screen()
+{
+
+}
+
+// close down current screen 
+void down_screen()
+{
+
+}
+
+// MAIN ROUTINE
+
 int main(int argc, char* argv[]) {
+
+    // setup    
+    start_pointer->prev = NULL;
+    start_pointer->next = NULL;
+    last_block();
+
     return 0;
 }
+
